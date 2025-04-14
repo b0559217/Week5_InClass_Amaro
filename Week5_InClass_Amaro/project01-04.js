48cc271e45ee8026f7f62a6856b9d3ed093db891
b0559217 <b0559217@amarillocollege.com>
2025-04-14 15:41:24 -0500

    Add files via upload
    initial commit

---
+++ Week5_InClass_Amaro/project01-04.js
@@ -0,0 +1,24 @@
+/*  JavaScript 7th Edition
+    Chapter 1
+    Hands-On Project 1-4
+
+    Author: Britney Amaro
+    Date:   04/14/2025
+
+    Filename: project01-04.js
+*/
+
+//define variables for home and work addresses
+const homeStreet = "1 Main St."
+const homeCity = "Sicilia"
+const homeState = "MA"
+const homeCode = "02103"
+const workStreet = "15 Oak Ln."
+const workCity = "Central City"
+const workCode = "02104"
+
+const homeStreet = document.getElementById("street")
+const homeCity = document.getElementById("city")
+const homeState = document.getElementById("state")
+const homeCode = document.getElementById("code")
+
